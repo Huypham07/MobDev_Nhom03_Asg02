0726ef687bbedbfa078a65e23f3255b639d3516c
LTDoanh <125759416+LTDoanh@users.noreply.github.com>
2024-04-26 23:31:05 +0700

    First Commit

---
+++ app/src/main/java/com/example/asg02/AdminEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.asg02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_event);
+    }
+}
