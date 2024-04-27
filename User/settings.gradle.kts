e8655fff40348b9dcc971fd2a8067b3d1b9d8215
Huypham07 <86708207+Huypham07@users.noreply.github.com>
2024-04-28 02:34:35 +0700

    Divide project into 3 folders
    Divide the original project into 3 folders corresponding to 3 types of applications: admin, manager, user

---
+++ User/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MobDev_Nhom03_Asg02(User)"
+include(":app")
+ 
