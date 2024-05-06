01bbb8db6264044bbef578764ecd92b29fa87ea0
Oramus <22028036@vnu.edu.vn>
2024-05-06 17:05:22 +0700

    CreateManagerApp#4

--- Manager/app/src/main/java/com/example/asg02/ManagerAddMovieActivity.java
+++ Manager/app/src/main/java/com/example/asg02/ManagerAddMovieActivity.java
@@ -3,17 +3,23 @@ package com.example.asg02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class ManagerAddMovieActivity extends AppCompatActivity {
     ImageButton back;
+    ImageView selectPosterImageView, setPosterImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_add_movie);
         back = findViewById(R.id.backMovie);
+        selectPosterImageView = findViewById(R.id.selectPoster);
+        setPosterImageView = findViewById(R.id.setPoster);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
 
@@ -21,7 +27,22 @@ public class ManagerAddMovieActivity extends AppCompatActivity {
                 Intent intent = new Intent(ManagerAddMovieActivity.this, ManagerActivity.class);
                 startActivity(intent);
             }
-
         });
+        selectPosterImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            setPosterImageView.setImageURI(imageUri);
+        }
     }
 }
